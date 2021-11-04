5db5fb7b9253783a2d73f0e7edc9911bfc0f2c07
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2021-11-04 15:48:06 +0530

    feat: Complete initial implementation

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,23 @@ const Layout: React.FC = ({ children }) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>JSON Formatter</title>
+        <meta name="title" content="JSON Formatter" />
+        <meta
+          name="description"
+          content="Dead simple tool to format (small) JSON files"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="JSON Formatter" />
+        <meta
+          property="og:description"
+          content="Dead simple tool to format (small) JSON files in the browser"
+        />
+        <meta property="twitter:title" content="JSON Formatter" />
+        <meta
+          property="twitter:description"
+          content="Dead simple tool to format (small) JSON files in the browser"
+        />
+        <title>JSON Formatter</title>
       </Head>
       <body>
         <main>{children}</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: [
+    "./public/**/*.html",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
