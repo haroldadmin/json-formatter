cd9e4480576fe65a4df20d7549a2a3a56c4cb645
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2021-12-11 23:03:28 +0530

    feat: Add OG image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,30 +1,23 @@
 import Head from "next/head";
 import Footer from "./Footer";
 
+const Title = "JSON Formatter";
+const Description =
+  "Dead simple tool to format (small) JSON files in the browser";
+
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>JSON Formatter</title>
         <meta name="title" content="JSON Formatter" />
-        <meta
-          name="description"
-          content="Dead simple tool to format (small) JSON files"
-        />
+        <meta name="description" content={Description} />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="JSON Formatter" />
-        <meta
-          property="og:description"
-          content="Dead simple tool to format (small) JSON files in the browser"
-        />
-        <meta property="twitter:title" content="JSON Formatter" />
-        <meta
-          property="twitter:description"
-          content="Dead simple tool to format (small) JSON files in the browser"
-        />
-        <title>JSON Formatter</title>
+        <meta property="og:title" content={Title} />
+        <meta property="og:image" content="/preview.png" />
+        <meta property="og:description" content={Description} />
+        <title>{Title}</title>
       </Head>
       <body>
         <main>{children}</main>
